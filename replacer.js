3b6d08dc98ceb23aed220c1d17880320d723f75e
leha@papulovskiy.ru <leha@papulovskiy.ru>
2013-05-15 18:15:08 +0400

    Fades

--- replacer.js
+++ replacer.js
@@ -7,18 +7,16 @@ $.get(chrome.extension.getURL('titles.txt'), function(data) {
     replace();
 });
 
-
-
 var saves = [];
 function replace() {
     if (document.location.href.match('/more/')) {
 	$("div.news_article_content > h1").each(function() {
-	    console.log($(this));
 	    saves.push($(this).text());
-	    console.log(saves);
-	    console.log(titlesCount, Math.floor(Math.random() * titlesCount));
 	    var text = titles[Math.floor(Math.random() * (titlesCount)) + 1];
-	    $(this).text(text);
+	    $(this).fadeOut(400, function() {
+		$(this).text(text);
+		$(this).fadeIn();
+	    });
 	});
     }
 }
